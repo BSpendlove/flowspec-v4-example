61d3e37e74a3e7770f4523d979a5bba197c0f263
BSpendlove <brandon-spendlove@hotmail.co.uk>
2020-11-23 11:28:49 +0000

    Initial commit

---
+++ exabgp/cli.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+import subprocess
+
+"""
+This Flask App is just a test
+"""
+
+app = Flask(__name__)
+
+@app.route("/exabgp/cli/show_neighbor_summary", methods=["GET"])
+def show_neighbors_summary():
+    cmd = "exabgpcli show neighbor summary"
+
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+    return result.stdout
+
+@app.route("/exabgp/cli/announce/flowspec", methods=["POST"])
+def announce_flowspec():
+    if not request.is_json:
+        return {"error": True, "message": "Invalid message type, JSON should be used..."}
+
+    cmd = None
+    data = request.get_json()
+
+    if not "neighbor" in data:
+        return {"error": True, "message": "neighbor not supplied."}
+
+    if not "command" in data:
+        return {"error": True, "message": "command not found."}
+
+    cmd = "exabgpcli neighbor {} '{}'".format(data["neighbor"], data["command"])
+
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+    if not result:
+        return None
+
+    return {"error": False, "stdout": str(result.stdout)}
+
+@app.route("/exabgp/cli/version", methods=["GET"])
+def version():
+    cmd = "exabgpcli version"
+
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+    return result.stdout
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
